33ecf29e4667811017c8a5ac24202ffaf6d227ee
ejaakml <eximindotong@yahoo.com>
2020-11-29 12:19:30 +0700

    63

---
+++ src/pboif2/pkg10119061/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Rheiza
+ */
+public interface GarisLurus {
+     public int hitungGradien();
+}
